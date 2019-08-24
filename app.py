67258b689573c48f90b63b8485c47ac3b0b7c020
Watcharis <m.watcharis@gmail.com>
2019-08-25 00:57:24 +0700

    test

--- app.py
+++ app.py
@@ -1,38 +1,38 @@
 from flask import Flask, jsonify, request
-# from linebot import LineBotApi
-# from linebot.exceptions import LineBotApiError
-# from linebot.models import (
-#  MessageEvent, TextMessage, TextSendMessage, ImageSendMessage,
-#  SourceUser, SourceGroup, SourceRoom,
-#  TemplateSendMessage, ConfirmTemplate, MessageTemplateAction,
-#  ButtonsTemplate, URITemplateAction, PostbackTemplateAction,
-#  CarouselTemplate, CarouselColumn, PostbackEvent,
-#  StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
-#  ImageMessage, VideoMessage, AudioMessage,
-#  UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent
-# )
+ from linebot import LineBotApi
+ from linebot.exceptions import LineBotApiError
+ from linebot.models import (
+  MessageEvent, TextMessage, TextSendMessage, ImageSendMessage,
+  SourceUser, SourceGroup, SourceRoom,
+  TemplateSendMessage, ConfirmTemplate, MessageTemplateAction,
+  ButtonsTemplate, URITemplateAction, PostbackTemplateAction,
+  CarouselTemplate, CarouselColumn, PostbackEvent,
+  StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+  ImageMessage, VideoMessage, AudioMessage,
+  UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent
+ )
 
-# from pymongo import MongoClient
-# from databases import data_word
-# from function import ReplyMessages
+ from pymongo import MongoClient
+ from databases import data_word
+ from function import ReplyMessages
 
-# import base64
-# import hashlib
-# import hmac
-# import json
-# import requests
-# import pymongo
-# import inspect
-# import collections
-# import bson
-# from bson.codec_options import CodecOptions
+ import base64
+ import hashlib
+ import hmac
+ import json
+ import requests
+ import pymongo
+ import inspect
+ import collections
+ import bson
+ from bson.codec_options import CodecOptions
 
 
 
 
 app = Flask(__name__)
 
-# reply_message = ReplyMessages()
+ reply_message = ReplyMessages()
 
 
 @app.route("/api",methods =["GET"])
@@ -40,16 +40,15 @@ def api_root():
     return jsonify({"status":200})
 
 
-# @app.route("/api_lines",methods = ["GET"])
-# def api_lines():
-#     return reply_message.handles_message()
+ @app.route("/api_lines",methods = ["GET"])
+ def api_lines():
+     return reply_message.handles_message()
 
-# @app.route("/api_line",methods = ["POST"])
-# def api_line():
-#     return reply_message.handle_message()
+ @app.route("/api_line",methods = ["POST"])
+ def api_line():
+     return reply_message.handle_message()
     
 
-    
     
 
 
